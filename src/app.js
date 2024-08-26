f26645c432b656a8214b1f8695d045cceccf11dc
bappi12312 <ranamdbappihossain@gmail.com>
2024-08-26 23:13:10 +0600

    all setup completed

---
+++ src/app.js
@@ -0,0 +1,30 @@
+import express from "express"
+import cors from "cors"
+import cookieParser from "cookie-parser"
+
+const app = express()
+
+//use is used for middlewares and for configuration
+app.use(cors({
+    origin : process.env.CORS_ORIGIN,
+    credentials : true
+}))
+
+app.use(express.json({limit : "16kb"}))
+app.use(express.urlencoded({extended : true,
+    limit : "16kb"}
+))
+app.use(express.static("public"))
+app.use(cookieParser())
+
+//router import
+import userRouter from "./routes/user.routes.js"
+import playlistRouter from "./routes/playlist.routes.js"
+
+
+// //routes declaration
+// app.use("/api/v1/users",userRouter)
+// app.use("/api/v1/playlist",playlistRouter)
+
+
+export { app }
