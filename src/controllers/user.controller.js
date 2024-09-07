a6ec42d5ca0c82c33ee38187be04b35659e4af7f
bappi12312 <ranamdbappihossain@gmail.com>
2024-09-07 23:01:01 +0600

    register controller added

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,4 +1,55 @@
 import { User } from "../models/user.model.js";
 import jwt from "jsonwebtoken"
 import mongoose from "mongoose"
+import { uploadOncloudinary} from "../utils/cloudinary.js"
+import { ApiError } from '../utils/ApiError.js'
+import { asyncHandler } from '../utils/asyncHandler.js'
+import {ApiResponse} from "../utils/ApiResponse.js"
 
+
+const register = asyncHandler(async (req,res) => {
+  const {name,email,password} = req.body;
+
+  if(
+    [name,email,password].some((value) => value?.trim() === "")
+  ) {
+    throw new ApiError(400,"all fields must be required")
+  }
+
+  const existedUser = await User.findOne({
+    $or: [{email},{name}]
+  })
+
+  if(existedUser) {
+    throw new ApiError(409,'user already exists')
+  }
+
+ const profilePictureLocalPath = req.files?.profilePicture[0]?.path;
+
+ if(!profilePictureLocalPath) {
+  throw new ApiError(400, "profile picture is required")
+ }
+
+ const profilePicture = await uploadOncloudinary(profilePictureLocalPath)
+
+ if(!profilePicture) {
+  throw new ApiError(400, "profile picture is not available")
+ }
+
+ const user = await User.create({
+  name,
+  profilePicture: profilePicture.url || '',
+  email: email.toLowerCase(),
+  password
+ })
+
+ const createdUser = await User.findById(user._id).select("-password -refreshToken")
+ 
+ if(!createdUser) {
+  throw new ApiError(500, "Something went wrong while registering the user")
+ }
+
+ return res.status(201).json(
+  new ApiResponse(200, createdUser, "User registered successfully")
+)
+})
--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,28 +1,28 @@
 import jwt from 'jsonwebtoken'
-import { User } from '../models/user.model'
-import { ApiError } from '../utils/ApiError'
-import { asyncHandler } from '../utils/asyncHandler'
+import { User } from '../models/user.model.js'
+import { ApiError } from '../utils/ApiError.js'
+import { asyncHandler } from '../utils/asyncHandler.js'
 
-export const verifyJwt = asyncHandler(async(req,_,next) => {
+export const verifyJwt = asyncHandler(async (req, _, next) => {
   try {
-    const token = req.cookies.accessToken || req.header("Authorization")?.replace("Bearer ","")
+    const token = req.cookies.accessToken || req.header("Authorization")?.replace("Bearer ", "")
 
-    if(!token) {
-      throw new ApiError(401,"unauthorized access")
+    if (!token) {
+      throw new ApiError(401, "unauthorized access")
     }
 
-    const decodedToken = jwt.verify(token,process.env.ACCESS_TOKEN_SECRET)
+    const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
 
     const user = await User.findById(decodedToken?._id).select("_password -refreshToken")
 
-    if(!user) {
-      throw new ApiError(401,"unauthorized access")
+    if (!user) {
+      throw new ApiError(401, "unauthorized access")
 
     }
 
     req.user = user;
     next()
   } catch (error) {
-    throw new ApiError(401,error?.message || "Invalid Access Token")
+    throw new ApiError(401, error?.message || "Invalid Access Token")
   }
 })
